4db74c40abe624e92874316ac589560d41c0df39
Cesar Lachira <lachiracesar@gmail.com>
2025-06-16 11:24:08 -0500

    feature: interaction plugin

--- README.md
+++ README.md
@@ -4,5 +4,4 @@ Proyecto inicial react - full calendar
 
 
 - [Integración - React](https://fullcalendar.io/docs/react)
-- [Documentación](https://fullcalendar.io/docs) uses [SWC](https://swc.rs/)
-
+- [Documentación](https://fullcalendar.io/docs)
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,29 +1,40 @@
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
-import timeGridPlugin from '@fullcalendar/timegrid'
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
+import timeGridPlugin from "@fullcalendar/timegrid";
+import esLocale from '@fullcalendar/core/locales/es';
+import interactionPlugin from "@fullcalendar/interaction" // needed for dayClick
+import { addHours } from "date-fns";
 import { NavbarPage } from "../components/Navbar";
-
+const headerToolbar = {
+  left: "prev,next",
+  center: "title",
+  right: "dayGridMonth,timeGridWeek,timeGridDay", // user can switch between the two
+};
 export const CalendarPage = () => {
-    return(
-        <>
-            <NavbarPage />
-            <main className='container-fluid mb-4'>
-                 <FullCalendar
-                plugins={[ dayGridPlugin, timeGridPlugin ]}
-                initialView="dayGridMonth"
-                headerToolbar={
-                    {
-                        left: 'prev,next',
-                        center: 'title',
-                        right: 'dayGridMonth,timeGridWeek,timeGridDay' // user can switch between the two
-                    }
-                }
-                events={[
-                    { title: 'Reunión', start: '2025-06-16T10:28:00', end: '2025-06-16T12:28:00' },
-                    { title: 'Cumpleaños del jefe', date: '2025-06-17' }
-                ]}
-                />
-            </main>
-        </>
-    )
+
+const handleDateClick = (arg) => {
+     alert(arg.dateStr);
 }
+  return (
+    <>
+      <NavbarPage />
+      <main className="container-fluid mb-4">
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          initialView="dayGridMonth"
+          headerToolbar={headerToolbar}
+          locale={esLocale}
+          dateClick={handleDateClick}
+          events={[
+            {
+              title: "Reunión",
+              start: new Date(),
+              end: addHours(new Date(), 2),
+            },
+            { title: "Cumpleaños del jefe", date: "2025-06-17" },
+          ]}
+        />
+      </main>
+    </>
+  );
+};
